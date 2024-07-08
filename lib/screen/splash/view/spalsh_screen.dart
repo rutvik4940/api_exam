c0b4204f35cd4fed66dcfa503f8ba87a920cb111
sumitmakasana <110807775+sumitmakasana@users.noreply.github.com>
2024-07-08 13:44:43 +0530

    final

--- lib/screen/splash/view/spalsh_screen.dart
+++ lib/screen/splash/view/spalsh_screen.dart
@@ -12,7 +12,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Future.delayed(Duration(seconds: 3),() => Navigator.pushNamed(context, 'home'),);
+    Future.delayed(Duration(seconds: 3),() => Navigator.pushReplacementNamed(context, 'home'),);
   }
   @override
   Widget build(BuildContext context) {
